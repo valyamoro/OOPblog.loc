c37d2f1fd6f90450584db479435cfee91aade205
kutlumbek <Moralek228@gmail.com>
2024-01-25 05:40:39 +0300

    dev fix (Теперь при регистрации ошибки валидации возвращаются в представление, а не передаются в сессию)

--- Services/Users/AddUserService.php
+++ Services/Users/AddUserService.php
@@ -8,22 +8,23 @@ use app\Services\BaseService;
 
 class AddUserService extends BaseService
 {
-    public function add(array $data): void
+    public function add(array $data): array
     {
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            // Переименовать. result плохое название.
-            $result = new UserModel(...$data);
-            $result->validator->setRules($result->rules());
+        $messages = [];
 
-            // Сначала false
-            if ($result->validator->validate($result)) {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $model = new UserModel(...$data);
+            $model->validator->setRules($model->rules());
+
+            if (!$model->validator->validate($model)) {
+                $messages = $model->validator->errors;
+            } else {
                 $now = \date('Y-m-d H:i:s');
 
                 $phoneNumber = \str_replace(['+', '8'], '', $data['phoneNumber']);
-                if (strlen($phoneNumber) === 10 && !str_starts_with($phoneNumber, '7')) {
+                if (\strlen($phoneNumber) === 10 && !\str_starts_with($phoneNumber, '7')) {
                     $phoneNumber = '7' . $phoneNumber;
                 }
-
                 $data = [
                     'firstName' => $data['firstName'], // 48
                     'lastName' => $data['lastName'], // 48
@@ -40,14 +41,12 @@ class AddUserService extends BaseService
                     $_SESSION['success'] = 'Вы успешно зарегистрировались!';
                     \header('Location: /home');
                 } else {
-                    // Возвращать result и передавать в вид ошибки.
                     $_SESSION['warning'] = 'Вы не зарегистрировались! Пожалуйста, попробуйте снова.';
                 }
-            } else {
-                $_SESSION['validate'] = $result->validator->errors;
             }
-
         }
+
+        return $messages;
     }
 
 }
--- Views/user/add.php
+++ Views/user/add.php
@@ -13,9 +13,9 @@
 
                     <input type="text" name="firstName" class="form-control" id="firstName"
                            aria-describedby="firstName">
-                    <?php if (isset($_SESSION['validate']['firstName'])): ?>
+                    <?php if (isset($validate['firstName'])): ?>
                         <div id="firstName"
-                             class="form-text"> <?php echo $_SESSION['validate']['firstName'][0]; ?> </div>
+                             class="form-text"> <?php echo $validate['firstName'][0]; ?> </div>
                     <?php else: ?>
                         <div id="firstName" class="form-text">We'll never share your firstName with anyone else</div>
                     <?php endif; ?>
@@ -27,9 +27,9 @@
                 <div class="mb-3">
                     <label for="lastName" class="form-label">Lastname</label>
                     <input name="lastName" class="form-control" id="lastName" aria-describedby="lastName"></input>
-                    <?php if (isset($_SESSION['validate']['lastName'])): ?>
+                    <?php if (isset($validate['lastName'])): ?>
                         <div id="lastName"
-                             class="form-text"> <?php echo $_SESSION['validate']['lastName'][0]; ?> </div>
+                             class="form-text"> <?php echo $validate['lastName'][0]; ?> </div>
                     <?php else: ?>
                         <div id="lastName" class="form-text">We'll never share your lastName with anyone else.</div>
                     <?php endif; ?>
@@ -39,9 +39,9 @@
                 <div class="mb-3">
                     <label for="Patronymic" class="form-label">Patronymic</label>
                     <input name="patronymic" class="form-control" id="Patronymic" aria-describedby="patronymic"></input>
-                    <?php if (isset($_SESSION['validate']['patronymic'])): ?>
+                    <?php if (isset($validate['patronymic'])): ?>
                         <div id="patronymic"
-                             class="form-text"> <?php echo $_SESSION['validate']['patronymic'][0]; ?> </div>
+                             class="form-text"> <?php echo $validate['patronymic'][0]; ?> </div>
                     <?php else: ?>
                         <div id="patronymic" class="form-text">We'll never share your patronymic with anyone else.</div>
                     <?php endif; ?>
@@ -54,8 +54,8 @@
                 <label for="email" class="form-label">Email address</label>
                 <input type="email" name="email" class="form-control" id="email"
                        aria-describedby="emailHelp">
-                <?php if (isset($_SESSION['validate']['email'])): ?>
-                    <div id="email" class="form-text"> <?php echo $_SESSION['validate']['email'][0]; ?> </div>
+                <?php if (isset($validate['email'])): ?>
+                    <div id="email" class="form-text"> <?php echo $validate['email'][0]; ?> </div>
                 <?php else: ?>
                     <div id="email" class="form-text">We'll never share your email with anyone else.</div>
                 <?php endif; ?>
@@ -64,8 +64,8 @@
                 <label for="phoneNumber" class="form-label">Phone number</label>
                 <input type="text" name="phoneNumber" class="form-control" id="phoneNumber"
                        aria-describedby="emailHelp">
-                <?php if (isset($_SESSION['validate']['phoneNumber'])): ?>
-                    <div id="phoneNumber" class="form-text"> <?php echo $_SESSION['validate']['phoneNumber'][0]; ?> </div>
+                <?php if (isset($validate['phoneNumber'])): ?>
+                    <div id="phoneNumber" class="form-text"> <?php echo $validate['phoneNumber'][0]; ?> </div>
                 <?php else: ?>
                     <div id="phoneNumber" class="form-text">We'll never share your phone number with anyone else.</div>
                 <?php endif; ?>
@@ -77,8 +77,8 @@
                 <label for="password" class="form-label">Password</label>
                 <input type="password" name="password" class="form-control" id="password"
                        aria-describedby="password_confirm">
-                <?php if (isset($_SESSION['validate']['password'])): ?>
-                    <div id="password" class="form-text"> <?php echo $_SESSION['validate']['password'][0]; ?> </div>
+                <?php if (isset($validate['password'])): ?>
+                    <div id="password" class="form-text"> <?php echo $validate['password'][0]; ?> </div>
                 <?php else: ?>
                     <div id="password" class="form-text">We'll never share your password with anyone else.</div>
                 <?php endif; ?>
@@ -87,8 +87,8 @@
                 <label for="passwordConfirm" class="form-label">Confirm your password</label>
                 <input type="password" name="passwordConfirm" class="form-control" id="passwordConfirm"
                        aria-describedby="passwordConfirm">
-                <?php if (isset($_SESSION['validate']['passwordConfirm'])): ?>
-                    <div id="password" class="form-text"> <?php echo $_SESSION['validate']['passwordConfirm'][0]; ?> </div>
+                <?php if (isset($validate['passwordConfirm'])): ?>
+                    <div id="password" class="form-text"> <?php echo $validate['passwordConfirm'][0]; ?> </div>
                 <?php else: ?>
                     <div id="password" class="form-text">We'll never share your password with anyone else.</div>
                 <?php endif; ?>
@@ -99,7 +99,6 @@
         <button type="submit" class="btn btn-primary">Registry</button>
     </form>
 </div>
-<?php unset($_SESSION['validate']) ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
         crossorigin="anonymous"></script>
