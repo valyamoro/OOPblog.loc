a7a512adae9178cc94134f7a2f3d411de029b67c
kutlumbek <Moralek228@gmail.com>
2024-02-01 08:58:48 +0300

    dev fix (Удалил errorhandler)

--- helpers/helpers.php
+++ helpers/helpers.php
@@ -12,16 +12,3 @@ function displayMessages(string $type): void
     echo \nl2br($_SESSION[$type]);
     unset($_SESSION[$type]);
 }
-
-function handleFatalError(): void
-{
-    $error = error_get_last();
-
-    if (!empty($error) && $error['type'] === E_ERROR) {
-        \header('Location: /errors/_404');
-        exit;
-    }
-
-}
-
-\register_shutdown_function('handleFatalError');
